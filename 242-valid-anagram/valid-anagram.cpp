152d8fb39e5524f0eb7754c956c3e40d4b5e7215
Shahid Nabi <128903863+shahidnabi-exe@users.noreply.github.com>
2024-11-30 08:53:57 +0500

    Time: 0 ms (100.00%) | Memory: 9.7 MB (41.06%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        
-        if(s.empty() || t.empty()) return false;
-
-        if (s.empty() && t.empty()) return false;
+    
 
         if(s.length() != t.length()){
             return false;
         }
         
-        // for (int i = 0; i < s.length(); i++){
-        //     for (int j = 0; j < t.length(); j++){
-        //         if(s[i] == t[j]){
-        //             break;
-        //         }
-        //         else continue;
-        //     }
+        int freq[26] = {0};
 
-        // }
-        // return true;
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        return s == t;
+        for(int i = 0; i < s.length(); i++){
+            freq[s[i] - 'a']++;
+            freq[t[i] - 'a']--;
+        }
+        for (int i=0; i<26; i++){
+            if(freq[i] != 0) return false;
+        }
+        return true;
+
+
+        //  short method
+       
+        // sort(s.begin(), s.end());
+        // sort(t.begin(), t.end());
+        // return s == t;
 
     }
 };
