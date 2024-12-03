c98f003f83675a6a894c23a70452d3bb9ef1bc7e
Shahid Nabi <128903863+shahidnabi-exe@users.noreply.github.com>
2024-12-03 06:52:30 +0500

    Time: 0 ms (100.00%) | Memory: 11.2 MB (6.59%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     bool check(vector<int>& nums) {
-        int count = 0; // To track the number of "drops"
+        int count = 0; 
         int n = nums.size();
 
         for (int i = 0; i < n; i++) {
-            // Compare current element with the next element (circularly)
             if (nums[i] > nums[(i + 1) % n]) {
                 count++;
                 if (count > 1) {
-                    return false; // More than one drop, invalid
+                    return false; 
                 }
             }
         }
         
-        return true; // One or zero drops are allowed
-    }
+        return true; 
+        }
 };
 
