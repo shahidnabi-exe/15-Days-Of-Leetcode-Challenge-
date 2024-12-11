a5c429f9b9f5caea5d9d8390e867ee375a6b17c6
Shahid Nabi <128903863+shahidnabi-exe@users.noreply.github.com>
2024-12-12 01:12:07 +0500

    Time: 0 ms (100.00%) | Memory: 23.7 MB (21.80%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,19 +1,34 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& arr) {
-        vector<int> temp; 
-        for (int i = 0; i < arr.size(); i++ ) {
-            if(arr[i] != 0) {
-                temp .push_back(arr[i]);
-            }
-        }    
-        for (int i = 0; i < arr.size(); i++ ) {
-            if(arr[i] == 0) {
-                temp .push_back(arr[i]);
-            }
-        }    
+        // vector<int> temp; 
+        // for (int i = 0; i < arr.size(); i++ ) {
+        //     if(arr[i] != 0) {
+        //         temp .push_back(arr[i]);
+        //     }
+        // }    
+        // for (int i = 0; i < arr.size(); i++ ) {
+        //     if(arr[i] == 0) {
+        //         temp .push_back(arr[i]);
+        //     }
+        // }    
 
-        arr = temp;  
-
+        // arr = temp;  
+        int j = -1;
+        for(int i = 0; i < arr.size(); i++) {
+            if(arr[i] == 0){
+                j = i;
+                break;
+            }
+        }
+        if (j == -1) return;
+        for( int i = j + 1; i < arr.size(); i++) {
+            if(arr[i] != 0){
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                j++;
+            }
+        }
     }
 };
