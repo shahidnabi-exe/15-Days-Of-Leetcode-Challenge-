ef3764404fa96e608c5eebf83e9f0203da29b522
Shahid Nabi <128903863+shahidnabi-exe@users.noreply.github.com>
2024-12-20 21:58:15 +0500

    Time: 2 ms (40.95%) | Memory: 28.3 MB (14.14%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,17 +1,31 @@
 class Solution {
 public:
     int majorityElement(vector<int>& arr) {
+        // Brute Force Solution
+        // for (int i = 0; i < arr.size(); i++) {
+        //     int count = 0;
+        //     for(int j = 0; j < arr.size(); j++) {
+        //         if (arr[i] == arr[j]) {
+        //             count++;
+        //             if(count > arr.size() / 2){
+        //                 return arr[i];
+        //             }
+        //         }
+        //     }
+        // }
+        // return -1;
+        // Better solution
+        unordered_map<int, int> map;
         for (int i = 0; i < arr.size(); i++) {
-            int count = 0;
-            for(int j = 0; j < arr.size(); j++) {
-                if (arr[i] == arr[j]) {
-                    count++;
-                    if(count > arr.size() / 2){
-                        return arr[i];
-                    }
-                }
+            map[arr[i]]++;
+        }
+
+        for(auto it : map) {
+            if(it.second > arr.size()/2) {
+                return it.first;
             }
         }
         return -1;
+
     }
 };
