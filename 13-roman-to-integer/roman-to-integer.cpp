7e63e06a6e24fdfd6dc036be37c396047543478f
Shahid Nabi <128903863+shahidnabi-exe@users.noreply.github.com>
2025-02-09 19:17:32 +0500

    Time: 2 ms (78.65%) | Memory: 10.1 MB (86.89%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -1,51 +1,34 @@
 class Solution {
 public:
-    int value(char r) {
-    if (r == 'I')
-        return 1;
-    if (r == 'V')
-        return 5;
-    if (r == 'X')
-        return 10;
-    if (r == 'L')
-        return 50;
-    if (r == 'C')
-        return 100;
-    if (r == 'D')
-        return 500;
-    if (r == 'M')
-        return 1000;
-    return -1;
-}
-    int romanToInt(string s) {
-         int res = 0; 
-
-    for (int i = 0; i < s.length(); i++) {
-        
-        // Get value of current symbol
-        int s1 = value(s[i]);
-
-        // Compare with the next symbol if asit exists
-        if (i + 1 < s.length()) {
-            int s2 = value(s[i + 1]);
-
-            // If current value is greater or equal, add it
-            // to result
-            if (s1 >= s2) {
-                res += s1;
-            }
-            else {
-                // Else, add the difference and skip next
-                // symbol
-                res += (s2 - s1);
-                i++;
-            }
-        }
-        else {
-            res += s1;
-        }
+    int val(char ch){
+        if(ch == 'I')   return 1;
+        else if(ch == 'V')    return 5;
+        else if (ch == 'L')   return 50;
+        else if(ch == 'X')    return 10;
+        else if(ch == 'M')    return 1000;
+        else if(ch == 'C')    return 100;
+        else if(ch == 'D')    return 500;        
+        else return -1;
     }
+    int romanToInt(string s) {
 
-    return res;
+        int res = 0;
+        for (int i = 0; i < s.length();  i++){
+
+            int s1 = val(s[i]);
+            if(i + 1 < s.length()) {
+
+                int s2 = val(s[i+1]);
+                if (s1 >= s2) {
+                    res += s1;
+                }
+                else {
+                    res += (s2 - s1);
+                    i++;    
+                }
+            }
+            else res += s1;
+        }
+        return res;
     }
 };
