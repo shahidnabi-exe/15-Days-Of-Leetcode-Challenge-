02d6d6df6d9ef8ef01a6d4faf8b545f34097662a
Shahid Nabi <128903863+shahidnabi-exe@users.noreply.github.com>
2025-02-20 01:04:16 +0500

    Time: 1075 ms (5.00%) | Memory: 20.6 MB (83.51%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int singleNumber(vector<int>& arr) {
-        for(int i = 0; i < arr.size(); i++) {
-            int count = 0;            
-            for ( int j = 0; j < arr.size(); j++) {
-                if (arr[j] == arr[i]) {
+        for (int i = 0; i < arr.size(); i++) {
+            int count = 0;
+            for(int j = 0; j < arr.size(); j++) {
+                if(arr[j] == arr[i]) {
                     count++;
                 }
-            }    
-            if(count == 1)   return arr[i];
+            }
+            if (count == 1) return arr[i];
         }
         return -1;
     }
