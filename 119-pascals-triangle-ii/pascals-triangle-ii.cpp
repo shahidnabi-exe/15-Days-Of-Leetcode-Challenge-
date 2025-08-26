e0cb3d59c8922022573c5fac7df9f2e486f81359
Shahid Nabi <128903863+shahidnabi-exe@users.noreply.github.com>
2025-08-26 17:41:24 +0500

    Time: 0 ms (100.00%) | Memory: 8.8 MB (77.32%) - LeetSync

--- 119-pascals-triangle-ii/pascals-triangle-ii.cpp
+++ 119-pascals-triangle-ii/pascals-triangle-ii.cpp
@@ -5,7 +5,7 @@ public:
         vector<int> v;
         v.push_back(1);
         for(int col = 1; col <= row; col++) {
-            ans = ans * (row - col+1);
+            ans = ans * (row - col + 1);
             ans = ans/(col);
             v.push_back(ans);
         }
